37425dc1007f3662260c2f2b489d6328b9c4b160
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2016-06-27 13:00:39 -0700

    Update Licenses

--- Vulkan/Vulkan/Renderer.cpp
+++ Vulkan/Vulkan/Renderer.cpp
@@ -15,6 +15,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include "Renderer.h"
 #include "util.h"
 #include "BUILD_OPTIONS.h"
